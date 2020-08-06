cf568527286946175b9796f234eadc39bccc0d6c
JoeL <jlawton1203@gmail.com>
2020-08-06 12:51:17 -0500

    created server.js and basic server setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.port || 3333;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+//routes
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.listen(PORT, () => {console.log(`API server now on port ${PORT}!`);}); 
