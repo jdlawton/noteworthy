4b445efaca6006de6d6f3f05eab733ce2c1f140d
JoeL <jlawton1203@gmail.com>
2020-08-07 10:23:16 -0500

    adding a new note will successfully POST to the app and update the db.json file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const {db} = require('./db/db.json');
+const uniqid = require('uniqid');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3333;
 
@@ -10,6 +12,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
+
+function addNewNote(body, noteList) {
+    let newNote = body;
+    db.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({db: noteList}, null, 2)
+    );
+}
 
 //routes
 app.get('/', (req,res) => {
@@ -23,6 +34,15 @@ app.get('/notes', (req,res) => {
 app.get('/api/notes', (req, res) => {
     let results = db;
     res.json(results);
+})
+
+app.post('/api/notes', (req, res) => {
+    //generate a new id using uniqid,
+    req.body.id = uniqid();
+    console.log(req.body);
+    res.json(req.body);
+    addNewNote(req.body, db);
+
 })
 
 app.listen(PORT, () => {console.log(`API server now on port ${PORT}!`);}); 
